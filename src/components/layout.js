6cf965f572c9512b524399511b8b0f167a3473ad
mint <analisnnic@gmail.com>
2020-03-01 15:34:56 +0200

    sign in singn up componets

---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Header from "./header/header";
+import Navbar from "./header/navbar";
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        {this.props.children}
+        <Header/>
+        <Navbar/>
+      </div>
+    );
+  }
+}
+
+export default Layout;
